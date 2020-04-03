e49a0812c14d90dd2bcb56619600ec9780a1a318
unknown <alexandre.ramt@gmail.com>
2020-04-02 20:54:39 -0400

    algo dij toutes les fonctionnalités faites, a debug!

--- TP1/main.cpp
+++ TP1/main.cpp
@@ -94,34 +94,68 @@ de Dijkstra, le plus court chemin entre un sommet et un autre. L’origine (point 
 et la destination (sommet d’arrivée) doivent être passées en paramètres. La fonction affiche le
 pourcentage final d’énergie dans les batteries de la voiture, le plus court chemin utilisé (d’après
 la liste de ses sommets, selon le format de l’annexe) et la longueur de ce dernier en minutes.*/
-/*
+
 void plusCourtChemin(int orig, int dest) {
 	int trajetsLongueurs[20]; // L
 	bool trajetsFaits[20]; // S
+
+	Chemin cheminsPris[1000]; // enregiste les chemins essayés
+	Chemin minICheminSommet;
+	Chemin minICheminTotal;
+
+	int o = 0;
+	int q = 20;
 	for (int i = 0; i < 20; i++) { trajetsLongueurs[i] = 99999; trajetsFaits[i] = 0;} // tous les chemins infinis et aucun trajet fait
 	trajetsLongueurs[orig] = 0; // orig a zero
 	trajetsFaits[orig] = 1; // sommet orig fait
 	while (trajetsFaits[dest] == false) {
-		for (int j = 0; j < 20; j++) { 
+		for (int j = 0; j < 20; j++) { //pour tous les sommets deja integrés
 			if (trajetsFaits[j]) {
 				int trajetMin = 99999;
 				int minID = j;
 				for (int k = 0; k < 20; k++) {
-					if (tableauChemins[j][k] < trajetsLongueurs[k] && trajetsFaits[k] = false) { // regarde les voisins non atteints de chaque sommets fait
-						if (tableauChemins[j][k] + trajetsLongueurs[j] < trajetMin){
+					if (tableauChemins[j][k] < trajetsLongueurs[k] && trajetsFaits[k] = false) { // regarde les voisins non integrés de chaque sommets fait
+						if (tableauChemins[j][k] + trajetsLongueurs[j] < trajetMin){ // on recupere le sommet non connu avec le chemin le plus court pour chaque sommet connu
 							trajetMin = tableauChemins[j][k] + trajetsLongueurs[j];
-							minID = k;
-						}
-						trajetsLongueurs[minID] = tableauChemins[j][minID] + trajetsLongueurs[j];
-						trajetsFaits[minID] = true;
+							minICheminSommet = Chemin(tableauChemins[j][k], new Arrondissement(j, liste_chemins[j].a_recharge_), new Arrondissement(k, liste_chemins[k].a_recharge_));
+						}						
 					}
 				}
+				if (minICheminSommet.get_trajet() < minICheminTotal.get_trajet()) { minICheminTotal = minICheminSommet; } // meilleur sommet inconnu voisin de tous les sommets connus
+				trajetsLongueurs[minICheminTotal.get_destination()->getNumero()] = tableauChemins[j][minICheminTotal.get_destination()->getNumero()] + trajetsLongueurs[j]; // om met a jour sa longueur
+				trajetsFaits[minICheminTotal.get_destination()->getNumero()] = true; // on l'ajout aux soommets connnus
+				cheminsPris[o++] = minICheminTotal;  // on le met dans le tableau de chemins essayés
 			}
 		}
+	} // destination atteinte, maintenant donner le chemin precis
+	bool bonChemin = false;
+	int batterie = 100;
+	int minutes = 0;
+	int listeFinale[20];
+	int y = o;
+	int x = 1;
+	int derniereDest = cheminsPris[o].get_origine()->getNumero();
+	listeFinale[0] = cheminsPris[o].get_destination()->getNumero();
+	minutes += cheminsPris[o].get_trajet();
+	while (!bonChemin)
+	{
+		while (cheminsPris[y - 1].get_destination()->getNumero() != derniereDest)
+		{
+			y--;
+		}
+		minutes += cheminsPris[y - 1].get_trajet(); 
+		if (cheminsPris[y-1].get_origine()->getNumero() == orig) {
+			bonChemin = true;
+			listeFinale[x] = orig;
+			break;
+		}
+		listeFinale[x++] = cheminsPris[y - 1].get_origine()->getNumero();
+		derniereDest = cheminsPris[y - 1].get_origine()->getNumero();
 	}
+	batterie -= minutes; //donner le pourcentage de batterie qui reste
 }
 
-*/
+
 
 /*Écrire la fonction ”traiterRequetes()” qui permet de déterminer, en vous inspirant de l’algorithme
 de Dijkstra, la faisabilité de requêtes de clients(nombre maximum de quatre clients à la fois à
