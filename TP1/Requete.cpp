670e4966de71009ccf4d667aefefb37528afeacc
unknown <autodesport@gmail.com>
2020-04-04 22:02:46 -0400

    lecture requetes

--- TP1/Requete.cpp
+++ TP1/Requete.cpp
@@ -13,6 +13,28 @@ Requete::Requete() {
 }
 
 void Requete::LireRequete(const string& Fichier) {
+	char separateur = ',';
+	string LigneAvecVirgule;
+	vector<int> requetes;
+	string LigneSansVirgule;
+	ifstream fichierALire(Fichier);
+
+	if (fichierALire.fail())
+		cout << "Probleme d'ouverture!" << endl;
+	else {
+		int i = 0;
+		while (!ws(fichierALire).eof()) {
+			getline(fichierALire, LigneAvecVirgule);
+			stringstream ChaineDeCaractere(LigneAvecVirgule);//brise la chaine de caractere
+			while (getline(ChaineDeCaractere, LigneSansVirgule, separateur)) {
+				requetes.push_back(stoi(LigneSansVirgule));//stoi: convertit string en int
+			}
+		}
+		for (auto it : requetes) {
+			cout << it << endl;
+		}
+		fichierALire.close();
+	}
 
 
 }
